5988537beec33cdecf8000d4222d66da61fd569d
Parth Saraswat <parthsaraswat@parths-mbp.home>
2020-05-30 23:25:44 -0400

    new post

--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Hey visitor! Thanks for visiting my website! This is my first post, and I want to outline what I’m trying to achieve with this website. As I get closer and closer to finishing up my Master’s degree, I want to ensure I keep learning new things and continue to keep...","categories": ["general"],
         "tags": ["content","css","edge case","lists","markup"],
         "url": "http://localhost:4000/general/2020/05/16/hello-world.html"
+      },{
+        "title": "Saying Goodbye To Cornell",
+        "excerpt":"It’s safe to say that there has always been a gap between the person I am and the person I want to be. I believe most people feel this way too. I am at point A, and I want to be at point B. There have always been guidelines on...","categories": ["general"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "http://localhost:4000/general/2020/05/30/saying.html"
       }]
