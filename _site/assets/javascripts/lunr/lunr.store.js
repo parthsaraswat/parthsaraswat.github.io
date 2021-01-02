fa459a24544d7bc4c25832ba479bec33951d9c59
Parth Saraswat <pspps3dsiwii@gmail.com>
2021-01-01 21:06:08 -0800

    date

--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -9,6 +9,11 @@ var store = [{
         "tags": ["daily","learninpublic","journal"],
         "url": "http://localhost:4000/daily/2020-08-24/"
       },{
+        "title": "January 1, 2021",
+        "excerpt":"Announcement! I just released a new podcast I have been working on with my friend! We are super excited about it, and you can find the first episode and trailer here! A good way to start the year. Writing What I did: Typed out the outline for the next post...","categories": ["daily"],
+        "tags": ["daily","learninpublic","journal"],
+        "url": "http://localhost:4000/daily/2021-01-01/"
+      },{
         "title": "August 11, 2020",
         "excerpt":"What I did: Writing Published a new postMusic Recorded the instrumental section for a cover of “Somewhere Only We Know” by KeaneParth Saraswat · SOWK - instrumental climax Made good progress on a jingle I’m writing with a friend for a secret project!Technical Set up this Daily Dispatch page to...","categories": ["daily"],
         "tags": ["content","project","guide"],
@@ -28,6 +33,21 @@ var store = [{
         "excerpt":"What I did: A bunch of admin work that was important, urgent, and mindnumbingly boring. Other exciting things: Writing Successful recording session on a secret project I’m working on with a friendOther Set up a Discord server I have big plans for. Exciting! I will open up the invite link...","categories": ["daily"],
         "tags": ["daily","learninpublic","journal"],
         "url": "http://localhost:4000/daily/daily4/"
+      },{
+        "title": "November 7, 2020",
+        "excerpt":"Happy Saturday! Today I made a quick guitar chord progression and recorded random footage, put it together and called it a ‘project’. Here’s ‘Fall’, put together in 1 hour! Not all projects and compositions need to be long and well thought out. This was fun just because of how short...","categories": ["daily"],
+        "tags": ["daily","learninpublic","journal"],
+        "url": "http://localhost:4000/daily/daily5/"
+      },{
+        "title": "November 9, 2020",
+        "excerpt":"What I did: Writing There are 2 posts scheduled to come out in the next 2 weeks. I spent some time thinknig about what each post was going to be. I had the idea in my head and a basic seed of the content, but today was about populating the...","categories": ["daily"],
+        "tags": ["daily","learninpublic","journal"],
+        "url": "http://localhost:4000/daily/daily6/"
+      },{
+        "title": "January 1, 2021",
+        "excerpt":"Announcement! I just released a new podcast I have been working on with my friend! We are super excited about it, and you can find the first episode and trailer here! A good way to start the year. Writing What I did: Typed out the outline for the next post...","categories": ["daily"],
+        "tags": ["daily","learninpublic","journal"],
+        "url": "http://localhost:4000/daily/daily7/"
       },{
         "title": "Hello Website!",
         "excerpt":"Hey visitor! Thanks for visiting my website! This is my first post, and I want to outline what I’m trying to achieve with this website. As I get closer and closer to finishing up my Master’s degree, I want to ensure I keep learning new things and continue to keep...","categories": ["general"],
@@ -68,6 +88,16 @@ var store = [{
         "excerpt":"Applying to the US for a Master’s degree in a STEM major can be daunting. It isn’t that there is a lack of information, but the opposite. The internet is flooded with information about this - all of which seems relevant. The process can be overwhelming and stressful. This post...","categories": ["general"],
         "tags": ["guide","so you want to"],
         "url": "http://localhost:4000/general/2020/10/23/masters.html"
+      },{
+        "title": "Contextual Book Recommendations 2020",
+        "excerpt":"Most book recommendations are along the lines of ‘If you liked (book 1), you will like (book 2)’ or ‘this was a very well written book’. This is a perfectly good way to recommend books, but I also think the books people enjoy reading depends on what sort of mood/state...","categories": ["general"],
+        "tags": [],
+        "url": "http://localhost:4000/general/2020/11/01/bookrecs.html"
+      },{
+        "title": "How to Approach Side Projects",
+        "excerpt":"Almost everyone has great ideas for side projects. Over the past 3-4 years, I have had numerous good ideas, but have only started on a fraction of them, finished even fewer, and been happy with maybe 1 or 2 out of 20. This post is an attempt at crystallizing what...","categories": ["general"],
+        "tags": ["guide","thoughts"],
+        "url": "http://localhost:4000/general/2020/11/13/side-proj.html"
       },{
         "title": "Branch Predictor FL models",
         "excerpt":"In computer architecture, a branch predictor is a digital circuit that tries to guess which way a branch (e.g. an if–then–else structure) will go before this is known definitively. The purpose of the branch predictor is to improve the flow in the instruction pipeline. Branch predictors play a critical role...","categories": [],
